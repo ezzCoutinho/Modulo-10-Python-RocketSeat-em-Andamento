bec3538a049f9f821d645e02fab47d5a106a4db5
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-24 21:19:10 -0300

    Adiciona métodos para seleção de documentos na classe OrdersRepository, permitindo a busca de múltiplos e únicos documentos no banco de dados

--- src/repository/orders_repository.py
+++ src/repository/orders_repository.py
@@ -13,3 +13,13 @@ class OrdersRepository:
     def insert_list_of_documents(self, documents: List[Dict]) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(documents)
+
+    def select_many(self, doc_filter: Dict) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_filter)
+        return list(data)
+
+    def select_one(self, doc_filter: Dict) -> Dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find_one(doc_filter)
+        return data
