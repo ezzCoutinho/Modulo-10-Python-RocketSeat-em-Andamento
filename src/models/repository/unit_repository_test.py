e99e573a2946d678bcee88c085c7f2e9412f1b6e
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-29 19:57:14 -0300

    Adiciona novos testes para a classe OrdersRepository, incluindo teste para seleção de múltiplos registros com propriedades específicas e marca o teste de inserção como pulado.

--- src/models/repository/unit_repository_test.py
+++ src/models/repository/unit_repository_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.models.repository.orders_repository import OrdersRepository
 
 
@@ -11,6 +13,7 @@ class CollectionMock:
 
     def find(self, *args):
         self.find_attributes["args"] = args
+        return []
 
 
 class DbCollectionMock:
@@ -23,6 +26,7 @@ class DbCollectionMock:
         return self.collection_name
 
 
+@pytest.mark.skip(reason="SKIPPED")
 def test_insert_document():
     collection_mock = CollectionMock()
     db_collection_mock = DbCollectionMock(collection_mock)
@@ -34,3 +38,22 @@ def test_insert_document():
     print(collection_mock.insert_one_attributes)
 
     assert collection_mock.insert_one_attributes["dict"] == data
+
+
+# @pytest.mark.skip(reason="SKIPPED")
+def test_select_many_with_properties():
+    collection_mock = CollectionMock()
+    db_collection_mock = DbCollectionMock(collection_mock)
+    orders_repository = OrdersRepository(db_collection_mock)
+
+    data = {"testando": "find"}
+    orders_repository.select_many_with_properties(data)
+    print()
+    print(collection_mock.find_attributes)
+    print(collection_mock.find_attributes["args"][0])
+    print(collection_mock.find_attributes["args"][1])
+
+    expected_args = (data, {"_id": 0, "cupom": 0})
+    assert collection_mock.find_attributes["args"] == expected_args
+    assert collection_mock.find_attributes["args"][0] == data
+    assert collection_mock.find_attributes["args"][1] == {"_id": 0, "cupom": 0}
