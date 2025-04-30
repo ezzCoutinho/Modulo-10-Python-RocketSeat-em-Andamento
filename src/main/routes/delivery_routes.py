6c28ec412ff275411143124bac6f88c25b762d11
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-29 21:12:18 -0300

    Atualiza a rota de registro de pedidos para utilizar a classe HttpRequest, encapsulando os dados da requisição antes de imprimir o corpo da solicitação.

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, jsonify, request
 
+from src.main.http_types.http_request import HttpRequest
+
 delivery_routes_bp = Blueprint("delivery_routes", __name__)
 
 
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
-    print(request.json)
+    http_request = HttpRequest(body=request.json)
+    print(http_request.body)
     return jsonify({"message": "Order registered successfully"}), 201
