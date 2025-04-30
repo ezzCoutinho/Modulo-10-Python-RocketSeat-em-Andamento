270c8b0572f44912cda15d82da60f4d81d920ba1
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-29 21:12:11 -0300

    Adiciona a classe HttpRequest, que encapsula os dados de requisição HTTP, incluindo corpo, cabeçalhos e parâmetros opcionais.

---
+++ src/main/http_types/http_request.py
@@ -0,0 +1,13 @@
+from typing import Dict, Optional
+
+
+class HttpRequest:
+    def __init__(
+        self,
+        body: Optional[Dict] = None,
+        headers: Optional[Dict] = None,
+        params: Optional[Dict] = None,
+    ):
+        self.body = body
+        self.headers = headers
+        self.params = params
