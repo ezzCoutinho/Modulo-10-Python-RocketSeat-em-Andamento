07cecefce99e19536ca9542e593708a67ed8c969
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-01 11:15:57 -0300

    Adiciona o parâmetro opcional 'query' à classe HttpRequest para permitir o envio de parâmetros de consulta nas requisições HTTP.

--- src/main/http_types/http_request.py
+++ src/main/http_types/http_request.py
@@ -7,7 +7,9 @@ class HttpRequest:
         body: Optional[Dict] = None,
         headers: Optional[Dict] = None,
         params: Optional[Dict] = None,
+        query: Optional[Dict] = None,
     ):
         self.body = body
         self.headers = headers
         self.params = params
+        self.query = query
