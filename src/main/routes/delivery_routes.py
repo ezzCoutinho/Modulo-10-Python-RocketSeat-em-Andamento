dd499b23d0faa6b61e4f8c68948d3cf472c4155b
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-01 10:21:31 -0300

    Refatora a rota de registro de pedidos para utilizar o composer e tratar exceções, retornando respostas apropriadas em caso de erro.

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, jsonify, request
 
 from src.main.http_types.http_request import HttpRequest
+from src.composer.registry_order_composer import registry_order_composer
 
 delivery_routes_bp = Blueprint("delivery_routes", __name__)
 
 
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
-    http_request = HttpRequest(body=request.json)
-    print(http_request.body)
-    return jsonify({"message": "Order registered successfully"}), 201
+    try:
+        user_case = registry_order_composer()
+        http_request = HttpRequest(body=request.json)
+        response = user_case.registry(http_request)
+        return jsonify(response.body), response.status_code
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
