49beae08b502db440040341e797a58785ba11cfc
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-01 10:11:11 -0300

    Tornando a classse DBConnectionHandler privada, e atribuindo ela uma variável, para chamar ela no composer do OrderRegistrer

--- src/models/connection/connection_handler.py
+++ src/models/connection/connection_handler.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from src.configs.credentials import credentials
 
 
-class DBConnectionHandler:
+class __DBConnectionHandler:
     def __init__(self) -> None:
         self.__connection_string = f"mongodb://{credentials['username']}:{credentials['password']}@{credentials['host']}:{credentials['port']}"
         self.__database_name = "rocket_db"
@@ -16,3 +16,6 @@ class DBConnectionHandler:
 
     def get_db_connection(self) -> MongoClient:
         return self.__db_connection
+
+
+db_connection_handler = __DBConnectionHandler()
