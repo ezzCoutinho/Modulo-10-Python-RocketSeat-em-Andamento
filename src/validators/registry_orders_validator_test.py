c1150490dfdd0d7577514528b2db84cffbaa19dc
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-01 10:46:07 -0300

    Adiciona testes para o validador de pedidos de registro, incluindo casos de sucesso e erro, utilizando pytest.

---
+++ src/validators/registry_orders_validator_test.py
@@ -0,0 +1,35 @@
+import pytest
+
+from src.validators.registry_orders_validator import registry_orders_validator
+
+
+@pytest.mark.skip(reason="SKIPPED")
+def test_registry_orders_validator():
+    body = {
+        "data": {
+            "name": "João",
+            "address": "Rua limão",
+            "cupom": False,
+            "items": [
+                {"item": "Refrigerante", "quantidade": 2},
+                {"item": "pizza", "quantidade": 3},
+            ],
+        }
+    }
+    registry_orders_validator(body)
+
+
+def test_registry_orders_validator_with_errors():
+    body_with_errors = {
+        "data": {
+            "name": 123,
+            "address": "Rua limão",
+            "cupom": "False",
+            "items": [
+                {"item": "Refrigerante", "quantidade": 2},
+                {"item": "pizza", "quantidade": 3},
+            ],
+        }
+    }
+    with pytest.raises(Exception):
+        registry_orders_validator(body_with_errors)
